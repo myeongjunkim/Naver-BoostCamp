238ea992472345903a393d67da570622652b291e
myeongjunkim <ilikerunning@naver.com>
2022-07-20 01:40:33 +0900

    refactor: userCPU.sh

--- userCPU.sh
+++ userCPU.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+head=`cat /proc/stat |head -1`
+SPLIT=($(echo ${head} | tr " " "\n"))
+
+all_cpu=$((SPLIT[1]))
+user_cpu=$((SPLIT[2]))
+
+let user_cpu_percent=$user_cpu*100/$all_cpu
+echo "${user_cpu_percent}"
+
