94877639cbaaf24cb29907b7bd0a82fccdcb0ce0
myeongjunkim <ilikerunning@naver.com>
2022-07-21 18:18:11 +0900

    feat: stack& memory class

---
+++ memory.js
@@ -0,0 +1,19 @@
+
+class Stack {
+    constructor(stackSize) {
+        this.arr = new Array();
+        this.maxSize = stackSize;
+        this.size = 0;
+        this.pointer = 0;
+    };
+}
+
+
+class Memmory {
+
+   
+
+
+
+
+}
