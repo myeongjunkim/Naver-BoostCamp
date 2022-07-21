9cc87ab839ad8819d233e4ffa5f63644397d2517
myeongjunkim <ilikerunning@naver.com>
2022-07-21 18:18:38 +0900

    feat: init func

--- memory.js
+++ memory.js
@@ -11,7 +11,20 @@ class Stack {
 
 class Memmory {
 
-   
+    // constructor() {
+    //     this.baseAdress;
+    //     this.typeList = {}
+    // }
+
+    init = (stackSize, heapSize) => {
+        this.baseAdress = 0;
+        this.typeList = {};
+        this.stack = new Stack(stackSize);
+        this.heap = {};
+        this.heapSize = 0;
+        this.maxHeapSize = heapSize;
+        return this.baseAdress;
+    }
 
 
 
